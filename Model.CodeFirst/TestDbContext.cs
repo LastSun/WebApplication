6059509425044073cf7ca5f2c28077772f96c50d
陈薛涛 <lastsun@outlook.com>
2015-09-23 00:27:16 +0800

    20150923@home

---
+++ Model.CodeFirst/TestDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+
+namespace Model.CodeFirst
+{
+    public class TestDbContext : IdentityDbContext<TestUser>
+    {
+        public TestDbContext()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+    }
+
+    public class TestUser : IdentityUser
+    {
+
+    }
+}
